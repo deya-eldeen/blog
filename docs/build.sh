fd2e537ee2bd531265fd9153d13a4f6627dc0877
Deya Eldeen <3denator@gmail.com>
2024-07-29 15:25:18 +0300

    theme22

--- docs/build.sh
+++
@@ -1,6 +0,0 @@
-echo "-------- removing cache"
-rm -rf .jekyll-cache
-rm -rf docs
-
-echo "-------- building"
-bundle exec jekyll build --verbose
--- docs/run.sh
+++
@@ -1,2 +0,0 @@
-JEKYLL_BASEURL="" JEKYLL_URL="" bundle exec jekyll serve
-#open http://127.0.0.1:4000/blog/docs/
