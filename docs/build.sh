4f77a8e6443361cdfbe3e5d19b91d56fde76bd1b
Deya Eldeen <3denator@gmail.com>
2024-07-29 05:31:50 +0300

    docs folder

--- docs/build.sh
+++ docs/build.sh
@@ -1,11 +1,9 @@
 echo "-------- removing cache"
 rm -rf .jekyll-cache
-rm -rf _site
+rm -rf docs
 
 echo "-------- building"
 bundle exec jekyll build --verbose
 
 echo "-------- copying to root"
-cp -rf _site/* .
 rm -rf _site
-
