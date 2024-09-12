3ea689c2f05dfb47f1605c5a36aecb2efd0b492c
Deya Elkhawaldeh <delkhawaldeh@expediagroup.com>
2024-09-13 02:38:05 +0300

    build

--- docs/build.sh
+++ docs/build.sh
@@ -3,5 +3,8 @@ rm -rf .jekyll-cache
 rm -rf docs
 echo "swiftbydeya.com" > docs/CNAME
 
+cp sitemap.xml docs/sitemap.xml
+cp robots.txt docs/robots.txt
+
 echo "-------- building "
 bundle exec jekyll build --verbose
